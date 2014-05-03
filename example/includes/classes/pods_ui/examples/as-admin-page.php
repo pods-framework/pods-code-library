9d436b5c349ba3db60ff48c1bbd8ce42631a3acf
Josh Pollock <jpollock412@gmail.com>
2014-05-03 17:08:57 -0400

    pods_ui example

--- example/includes/classes/pods_ui/examples/as-admin-page.php
+++ example/includes/classes/pods_ui/examples/as-admin-page.php
@@ -1,10 +1,36 @@
 <?php
 /**
- * @TODO What this does.
+ * This basic example is to illustrate functionality. It reproduces what Pods already does automatically.
+ */
+add_action( 'admin_menu', 'slug_admin_page' );
+function slug_admin_page() {
+	add_menu_page( 'CPT Admin', 'CPT Admin', 'edit_posts', 'menu-slug', 'slug_cpt_ui', 'dashicons-admin-site');
+
+}
+
+function slug_cpt_ui() {
+	$pods = pods('cpt');
+	return $pods->ui();
+}
+
+/**
+ * This example shows how to create an alternate UI for non-admin users with diminished capabilites.
+ * Be sure to set the third arg in add_menu_page to the right capability. See http://codex.wordpress.org/Roles_and_Capabilities
  *
- * @package   @TODO
- * @author    Josh Pollock <Josh@JoshPress.net>
- * @license   GPL-2.0+
- * @link      
- * @copyright 2014 Josh Pollock
- */ 
+ */
+add_action( 'admin_menu', 'slug_admin_page' );
+function slug_admin_page() {
+	add_menu_page( 'CPT Admin', 'CPT Admin', 'edit_posts', 'menu-slug', 'slug_cpt_ui', 'dashicons-admin-site');
+
+}
+
+/**
+ * Note that by setting the second arg to true in pods::ui() these new settings override existing values, but all other values set in Pods Admin remain.
+ */
+function slug_cpt_ui() {
+	$pods = pods('cpt');
+	$override_options['fields'] = array('post_title', 'post_date', 'custom_field');
+	$override_options[ 'actions_disabled' ] = array( 'edit', 'delete' );
+
+	return $pods->ui( $override_options, true);
+}
