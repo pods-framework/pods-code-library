0f99b152f1472f21017ddd0e23497bd5d65fcc26
Josh Pollock <jpollock412@gmail.com>
2014-06-14 15:27:26 -0400

    Fix last example to deal with IN() not working:(

--- example/misc/examples/fields-from-custom-taxonomy.php
+++ example/misc/examples/fields-from-custom-taxonomy.php
@@ -5,30 +5,39 @@
  * Designed to be used in single item view of a post type Pod associated with a custom taxonomy.
  */
 //set name of custom taxonomy we want to show fields of
-$taxonomy = 'genre';
+$taxonomy = 'genres';
 
-//get all terms of current post in that taxonomy
+//get all terms in taxonomy for current post
 global $post;
-$terms = wp_get_post_terms( $post->ID , $taxonomy );
-
-
+$terms = wp_get_post_terms( $post->ID, $taxonomy );
 
 //Put term ids in an array.
 foreach ( $terms as $term ) {
-	$ids[ ] = $term->term_id;
+	$ids[] = $term->term_id;
 }
 
-//query by term_id(s)
-$params = array (
-	'where' => 't.term_id IN( " ' . $ids . '" )',
-	'limit' => 5,
-);
+//build where clause to query by term_id(s)
+$i = 0;
+foreach( $ids as $id ) {
+	if ( $i === 0 ) {
+		$where = 't.term_id = "'.$id.'"';
+	}
+	else {
+		$where .= ' OR t.term_id = "'.$id.'"';
+	}
 
+	$i++;
+
+}
 
 //build pods object
-$taxonomy = pods( 'CUSTOM_TAXONOMY_NAME', $params );
+$params = array (
+	'where' => $where,
+	'limit' => 5,
+);
+$taxonomy = pods( $taxonomy, $params );
 
-//loop if there are matching terms, if there are not something ver wrong has heppened.
+//loop if there are matching terms, if there are not something ver wrong has happened.
 if ( $taxonomy->total() > 0 ) {
 	while ( $taxonomy->fetch() ) {
 
