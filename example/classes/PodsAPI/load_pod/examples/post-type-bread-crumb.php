c0dd9380d17a4729b065f6b4cd9e5c3de398d54f
Josh Pollock <jpollock412@gmail.com>
2014-06-30 20:56:32 -0400

    Basic CPT breadcrumbs, but handle possible error if CPT isn't a Pod.

--- example/classes/PodsAPI/load_pod/examples/post-type-bread-crumb.php
+++ example/classes/PodsAPI/load_pod/examples/post-type-bread-crumb.php
@@ -9,22 +9,32 @@ function slug_post_type_breadcrumb( $divider = '&gt;&gt;' ) {
 	$front = slug_link( site_url(), 'Home' );
 	if ( is_home() || is_front_page() ) {
 		return $front;
+
 	}
 
 	if( is_post_type_archive() || ( is_singular() && get_post_type() !== false ) ) {
 		$post_type = get_post_type();
 		$pod = pods_api()->load_pod( $post_type );
-		$single = $pod->options['single_label'];
-		$plural = slug_link( get_post_type_archive( $post_type), $pod->label, 'All '.$single );
+		if ( !is_string( $pod ) ) {
+			$single = $pod->options[ 'single_label' ];
+			$plural = slug_link( get_post_type_archive( $post_type ), $pod->label, 'All ' . $single );
 
 
-		if ( is_post_type_archive()  ) {
-			return $front . $divider . $plural;
+			if ( is_post_type_archive() ) {
+				return $front . $divider . $plural;
+
+			}
+
+			global $post;
+			$single = slug_link( get_permalink( $post->ID ), get_the__title( $post->ID ), 'View '.$single );
+			return $front . $divider . $single;
+
 		}
+		else {
 
-		global $post;
-		$single = slug_link( get_permalink( $post->ID ), get_the__title( $post->ID ), 'View '.$single );
-		return $front . $divider . $single;
+			return;
+
+		}
 	}
 
 }
