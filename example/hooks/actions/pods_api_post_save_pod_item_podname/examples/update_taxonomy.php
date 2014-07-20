09a97ff4e24090a71e020230a98e4ca947b2875b
Matteo Spinelli <matteo@cubiq.org>
2014-07-20 09:09:14 +0200

    Update update_taxonomy.php
    support for multi-select values

--- example/hooks/actions/pods_api_post_save_pod_item_podname/examples/update_taxonomy.php
+++ example/hooks/actions/pods_api_post_save_pod_item_podname/examples/update_taxonomy.php
@@ -1,19 +1,27 @@
 <?php
 /**
  * Update Taxonomy With Value Of Field Related To A Taxonomy
- * Takes the value of a single-select field 'genre' related to the taxonomy 'genres' and updates the taxonomy with the term set in the 'genre' field from the custom post type 'films'.
+ * Takes the value of a single- or multi-select field 'pods_genre' related to the taxonomy 'genre' and updates the taxonomy with the term set in the 'pods_genre' field from the custom post type 'films'.
  */
 add_action( 'pods_api_post_save_pod_item_films', 'my_tax_update', 10, 3 );
 
 function my_tax_update( $pieces, $is_new_item, $id ) {
 
-	//get the value of the 'genre' field
-	$term = (int) $pieces[ 'fields' ][ 'genre' ][ 'value' ];
+	// get the values of the 'pods_genre' field
+	$terms = $pieces[ 'fields' ][ 'pods_genre' ][ 'value' ];
 
-	//if there is nothing there set $term to null to avoid errors
-	if ( empty( $term ) ) { $term = null; }
+	if ( empty( $terms ) ) {
+		// if there is nothing there set $term to null to avoid errors
+		$terms = null;
+	} else {
+		// create an array out of the comma separated values
+		$terms = explode(',', $terms);
+		
+		// ensure all values in the array are integer (otherwise the numerical value is taken as a new taxonomy term instead of an ID)
+        	$terms = array_map('intval', $terms);
+	}
 
-	//Set the term for taxonomy 'genres' with $term
-	wp_set_object_terms( $id, $term, 'genres', false );
+	// Set the term for taxonomy 'genre' with $terms
+	wp_set_object_terms( $id, $term, 'genre', false );
 	
 }
