70331a8c3348e2941e411190f43ced122e617662
Josh Pollock <jpollock412@gmail.com>
2014-10-16 13:32:53 -0400

    remove extra pasted stuff.

--- example/misc/json-api/examples/prepare.php
+++ example/misc/json-api/examples/prepare.php
@@ -5,11 +5,4 @@
  * Requires https://github.com/WP-API/client-js
  */
 add_action( 'wp_enqueue_scripts', 'json_api_client_js' );
-add_action( 'wp_enqueue_scripts', 'json_api_talk_scripts' );
-function json_api_talk_scripts() {
-	if ( ! function_exists( 'json_get_url_prefix' ) ) {
-		return;
-	}
-	wp_enqueue_script( 'json-api-talk', plugins_url( '/json-api-talk.js', __FILE__ ), array( 'jquery' ), '1.0', true );
 
-}
