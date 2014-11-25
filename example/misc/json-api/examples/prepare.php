f1bebcee78a227872d8d29cb5ed1203cb1f1cc3a
Bernhard <quasel@users.noreply.github.com>
2014-11-25 01:24:16 +0100

    Update prepare.php
    Updated - this way we don't need WP-API/client.js

--- example/misc/json-api/examples/prepare.php
+++ example/misc/json-api/examples/prepare.php
@@ -4,9 +4,25 @@
  * Example for get-by-id.js - assuming its stored in your plugin Directory in /js
  * 
  */
-wp_enqueue_script('get-by-id', plugins_url('js/get-by-id.js', __FILE__), array('jquery'), true);
+ 
+//api-access for frontend" (only logged in Users) 
+add_action( 'wp_enqueue_scripts', 'your_prefix_json_api_js' );
 
-//only necesarry once if you have multiple .js files 
-$settings = array( 'root' => esc_url_raw( get_json_url() ), 'nonce' => wp_create_nonce( 'wp_json' ) );
-wp_localize_script( 'get-by-id', 'WP_API_Settings', $settings );
+//api-access for wp-admin
+add_action( 'admin_enqueue_scripts', 'your_prefix_json_api_js' );
 
+function your_prefix_json_api_js{ 
+  /**
+	 * Check if WP API functionality exists. Not using is_plugin_active in prepartion for
+	 */
+	if ( ! function_exists( 'json_get_url' ) ) {
+		return;
+	}
+  
+  wp_enqueue_script('get-by-id', plugins_url('js/get-by-id.js', __FILE__), array('jquery'), true);
+
+  //only necessary once if you have multiple .js files 
+  $settings = array( 'root' => esc_url_raw( get_json_url() ), 'nonce' => wp_create_nonce( 'wp_json' ) );
+  wp_localize_script( 'get-by-id', 'WP_API_Settings', $settings );
+  
+}
