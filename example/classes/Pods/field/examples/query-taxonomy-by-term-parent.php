de71cce2098d2243262d81380b91022e872e857c
Josh Pollock <jpollock412@gmail.com>
2015-02-02 15:19:00 -0500

    More explanation of the differences.

--- example/classes/Pods/field/examples/query-taxonomy-by-term-parent.php
+++ example/classes/Pods/field/examples/query-taxonomy-by-term-parent.php
@@ -12,5 +12,36 @@
 //Using Pods class
 $pods = pods( 'download_category', array( 'tt.parent = "6"' ) );
 
-//Using get_terms
+//Using get_terms()
 $terms = get_terms( 'download_category', array( 'parent' => 6 ) );
+
+
+/**
+ * Examples illustrating the diffrences described, pressuming the existance of a custom field called "word_count"
+ */
+ //Using Pods class
+$pods = pods( 'download_category', array( 'tt.parent = "6"' ) );
+if ( $pods->total() > 0 ) {
+  while( $pods->fetch()  {
+    //echo the term name
+    echo $pods->display( 'name' );
+    
+    //echo the custom field
+    echo $pods->display( 'word_count' );
+  }
+  
+}
+
+//Using get_terms()  
+/* IMPORTANT: For demonstration purposes only, will not work */
+$terms = get_terms( 'download_category', array( 'parent' => 6 ) );
+foreach( $terms as $term ) {
+  //echo the term name
+  echo $term->name;
+  
+  //try to echo the custom field like this and you will get an ERROR
+  echo $term->word_count;
+  
+}
+  
+}
